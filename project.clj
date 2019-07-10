64f49493ff9256724f3db13c127b2d5cc02509bd
Robert Fisher <services@id264.net>
2019-07-10 14:05:17 +0100

    retry main

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Stupid fatuous random string thing"
   :url "https://meetup.sysdef.xyz"
   :min-lein-version "2.0.0"
-  :main "src/meetup_generator/handler.clj"
+  :main "meetup-generator.handler"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.forward/yaml "1.0.9"]
                  [compojure "1.6.1"]
