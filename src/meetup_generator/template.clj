f00a99f44483fa0f7364e6449111b6c7742b5013
Robert Fisher <services@id264.net>
2019-12-04 23:07:15 +0000

    spacing fixes

--- src/meetup_generator/template.clj
+++ src/meetup_generator/template.clj
@@ -28,12 +28,12 @@
            [:h1 "#DevOps Meetup // Shoreditch, probably // " (.format formatter tomorrow) ]
            [:ul
             [:li "18:00 // Introduction"]
-            (talk "18:10" "Lightning Talk //" content 0)
+            (talk "18:10" "Lightning Talk // " content 0)
             (talk "18:20" nil content 1)
             [:li "18:50 // break"
               [:div {:class "tperson"} (:refreshment content)]]
             (talk "19:20" nil content 2)
-            (talk "19:40" "Ignite //" content 3)
+            (talk "19:40" "Ignite // " content 3)
             (talk "20:00" nil content 4)
             [:li "20:30 // Close"
              [:div {:class "tperson"}
